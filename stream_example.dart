21ff0a1087fc35e5841879d9f40303238db814c6
PragnaBhatt <pragnavbhatt@gmail.com>
2022-09-11 23:18:31 +0530

    Initial commit

---
+++ stream_example.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+class Cake{}
+class Order{
+  
+  String type;
+  Order(this.type);
+}
+
+main(){
+  
+
+  final controller=StreamController();
+    Order order=new Order('chocolate');
+  print(order.type);
+  controller.sink.add(order);
+  
+  final baker=new StreamTransformer.fromHandlers(handleData: (cakeData,sink)
+                                                 {if(cakeData=='chocolate'){
+                                                   
+                                                   sink.add(new Cake());
+                                                 }
+                                                 else{
+                                                   sink.addError('we can not create this cake');
+                                                 }}
+                                                );
+    
+  controller.stream.map((order)=>order.type).transform(baker).listen((event) {
+    print(event);
+  },onError: (err)=>print(err));
+}
