bc265fc7766f0352d27a3a22343577aa59e5bf6e
PragnaBhatt <cleancodersofficial@gmail.com>
2022-09-11 23:38:51 +0530

    Update stream_example.dart

--- stream_example.dart
+++ stream_example.dart
@@ -1,31 +1,28 @@
 import 'dart:async';
 
-class Cake{}
-class Order{
-  
+class Cake {}
+
+class Order {
   String type;
   Order(this.type);
 }
 
-main(){
-  
-
-  final controller=StreamController();
-    Order order=new Order('chocolate');
+main() {
+  final controller = StreamController();
+  Order order = new Order('chocolate');
   print(order.type);
   controller.sink.add(order);
-  
-  final baker=new StreamTransformer.fromHandlers(handleData: (cakeData,sink)
-                                                 {if(cakeData=='chocolate'){
-                                                   
-                                                   sink.add(new Cake());
-                                                 }
-                                                 else{
-                                                   sink.addError('we can not create this cake');
-                                                 }}
-                                                );
-    
-  controller.stream.map((order)=>order.type).transform(baker).listen((event) {
+
+  final baker =
+      new StreamTransformer.fromHandlers(handleData: (cakeData, sink) {
+    if (cakeData == 'chocolate') {
+      sink.add(new Cake());
+    } else {
+      sink.addError('we can not create this cake');
+    }
+  });
+
+  controller.stream.map((order) => order.type).transform(baker).listen((event) {
     print(event);
-  },onError: (err)=>print(err));
+  }, onError: (err) => print(err));
 }
